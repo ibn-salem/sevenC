aa9ddacc893b84dd66ba19dda807ff54566c248d
ibn-salem <jonas.ibn-salem@gmx.de>
2017-02-16 20:11:59 +0100

    draft addCovToGR function and associated tests

--- R/parseChIP-seq.R
+++ R/parseChIP-seq.R
@@ -9,7 +9,8 @@
 #' @param inFile Input file path or connection. See \code{con} paramter in
 #' \code{\link[rtracklayer]{import}} function.
 #' @param seqInfo A \link{\code{seqinfo}} object defining the reference genome.
-#' @return An RLE object with density values for each position in the genome.
+#' @return An \code{\link{RleList}} object with density values for each
+#' position in the genome.
 #' @export
 parseBigWigToRle <- function(inFile, seqInfo, format="bigWig"){
 
@@ -23,4 +24,33 @@ parseBigWigToRle <- function(inFile, seqInfo, format="bigWig"){
   # compute coverage by using the score as weight
   bwCov = GenomicRanges::coverage(bwGR, weight="score")
   return(bwCov)
+}
+
+
+#' Add coverage to regions in \code{\link[GenomicRanges]{GRanges}} object.
+#'
+#' This function adds a coverage vector to each range in a genomic ranges
+#' object. The coverage is reported for a fixed-sized window around the region
+#' center and is reversed in case of negative strand of the region.
+#'
+#' @param gr \code{\link[GenomicRanges]{GRanges}} object with genomic regions
+#' @param cov \code{\link{RleList}} object with coverage for each chromosome. Such
+#'   an object is returned from \code{\link{parseBigWigToRle}} function.
+#' @param window the window size arund the center of ranges in \code{gr}.
+#' @param bin_size size of bins to which the coverage values are combined.
+#' @return \code{\link[GenomicRanges]{GRanges}} as input but with an additional meta column containing the coverage values for each region.
+#'
+addCovToGR <- function(gr, cov, window=1000, bin_size=10){
+
+  # get windows around gr
+  ancWin <- GenomicRanges::resize(gr, width=window, fix="center")
+
+  covGRL <- GenomicRanges::ranges(cov)
+
+  ancWinR <- GenomicRanges::restrict(ancWin, covGRL)
+
+  # get numeric with coverage of each region
+  covAnc <- IRanges::NumericList(cov[ancWin])
+
+
 }
--- tests/testthat/test-parseChIP-seq.R
+++ tests/testthat/test-parseChIP-seq.R
@@ -1,14 +1,15 @@
 context("parseChIP-seq")
 
 
+exampleBigWig <- system.file("extdata", "GM12878_Stat1.chr22_1-18000000.bigWig", package="chromloop")
+
+toySeqInfo <- GenomeInfoDb::Seqinfo(seqnames=c("chr1", "chr22"),
+                                    seqlengths=c(10^8, 10^8),
+                                    isCircular=c(FALSE, FALSE),
+                                    genome="toy")
+
+
 test_that("parseBigWigToRle on example dataset", {
-
-  exampleBigWig <- system.file("extdata", "GM12878_Stat1.chr22_1-18000000.bigWig", package="chromloop")
-
-  toySeqInfo <- GenomeInfoDb::Seqinfo(seqnames=c("chr1", "chr22"),
-                                      seqlengths=c(10^8, 10^8),
-                                      isCircular=c(FALSE, FALSE),
-                                      genome="toy")
 
   bwCov <- parseBigWigToRle(exampleBigWig, toySeqInfo)
 
@@ -17,3 +18,24 @@ test_that("parseBigWigToRle on example dataset", {
   expect_equal(length(as.vector(bwCov$chr22)), 100000000)
 
 })
+
+
+test_that("addCovToGR on example dataset", {
+
+  cov <- IRanges::RleList(list(
+    "chr1"=rep(0, 1, 0, each=20),
+    "chr22"=rep(c(0, 5, 1, 5, 0), each=10)
+  ))
+
+  gr <- GenomicRanges::GRanges(
+    rep("chr1", 3),
+    IRanges::IRanges(
+      c(0, 10, 10),
+      c(5, 10, 20)
+    )
+  )
+
+  grCov <- addCovToGR(gr, cov, window=10, bin_size=1)
+
+})
+
