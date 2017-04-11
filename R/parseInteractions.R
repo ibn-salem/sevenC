139123e54c94d5e5170fb8f21929b26e1367dc4a
ibn-salem <jonas.ibn-salem@gmx.de>
2017-04-11 18:00:55 +0200

    add parseLoopsTang2015() function with example data and tests

--- R/parseInteractions.R
+++ R/parseInteractions.R
@@ -1,12 +1,12 @@
 
 
-#' Parse chromatin loops from Rao et al 2014 as an \code{\link{InteractionSet}}
+#' Parse chromatin loops from Rao et al 2014 as an \code{\link{GInteractions}}
 #' object.
 #'
-#' @param infile input file with loops
+#' @param inFile input file with loops
 #' @param ... additional arguments, that will be passed to
 #'   \code{\link{GRanges()}} functions.
-#' @return An code{\link{InteractionSet}} with loops from input file.
+#' @return An \code{\link{GInteractions}} with loops from input file.
 #' @export
 parseLoopsRao <- function(inFile, ...){
 
@@ -19,16 +19,16 @@ parseLoopsRao <- function(inFile, ...){
   # substract 1 bp from down coordinate to have inclusive interval ranges
   upAnchor = GenomicRanges::GRanges(
       chr,
-      IRanges::IRanges(raoDF[,"x1"], raoDF[,"x2"]-1),
+      IRanges::IRanges(raoDF[,"x1"], raoDF[,"x2"] - 1),
       ...)
 
   downAnchor = GenomicRanges::GRanges(
     chr,
-    IRanges::IRanges(raoDF[,"y1"], raoDF[,"y2"]-1),
+    IRanges::IRanges(raoDF[,"y1"], raoDF[,"y2"] - 1),
     ...)
 
   # build GInteractions
-  gi <- InteractionSet::GInteractions(upAnchor, downAnchor, mode="strict")
+  gi <- InteractionSet::GInteractions(upAnchor, downAnchor, mode = "strict")
 
   # define and add additional annotations to each interaction
   annotationCols <- setdiff(names(raoDF),
@@ -38,3 +38,55 @@ parseLoopsRao <- function(inFile, ...){
 
   return(gi)
 }
+
+
+#'Parse chromatin interactions from Tang et al 2015 as \code{GInteractions}.
+#'
+#'Reads pairwise ChIA-PET interaction from an input file.
+#'
+#'It reads files with the follwoing tab-delimited format:
+#'
+#'\tabular{lllllll}{ chr12\tab 48160351\tab 48161634\tab  chr12\tab 48230665\tab
+#'48232848\tab 27 \cr chr7\tab 77284664\tab 77285815\tab chr7\tab  77388242\tab
+#'77388928\tab 7 \cr chr4\tab 128459961\tab 128460166\tab chr4\tab
+#'128508304\tab 128509082\tab 4 \cr }
+#'
+#'This file format was used for ChIA-PET interaction data by Tang et al. 2015
+#'\url{http://dx.doi.org/10.1016/j.cell.2015.11.024}. The last column of input
+#'file is added as annotation column with colname "score".
+#'
+#'@param inFile input file with loops
+#'@param ... additional arguments, that will be passed to
+#'  \code{\link{GRanges()}} functions.
+#'@return An \code{\link{GInteractions}} with loops from input file.
+#'@export
+parseLoopsTang2015 <- function(inFile, ...){
+
+
+
+  message("INFO: Parse ChiA-PET interactions from file: ", inFile)
+
+  # parse IMR90 domains from Rao et al 2014:
+  inDF = read.delim(inFile, header = FALSE)
+
+  # substract 1 bp from down coordinate to have inclusive interval ranges
+  upAnchor = GenomicRanges::GRanges(
+    inDF[,1],
+    IRanges::IRanges(inDF[,2], inDF[,3]), ...)
+
+  downAnchor = GenomicRanges::GRanges(
+    inDF[,4],
+    IRanges::IRanges(inDF[,5], inDF[,6]), ...)
+
+
+  # build GInteractions
+  gi <- InteractionSet::GInteractions(upAnchor, downAnchor, mode = "strict")
+
+  # annotate GInteractions object with score
+  gi$score <- inDF[,7]
+
+  return(gi)
+
+}
+
+
--- tests/testthat/test-parseInteractions.R
+++ tests/testthat/test-parseInteractions.R
@@ -3,16 +3,18 @@ context("parseInteractions")
 
 exampleLoopFile <- system.file("extdata", "GM12878_HiCCUPS.chr22_1-18000000.loop.txt", package="chromloop")
 
-toySeqInfo <- GenomeInfoDb::Seqinfo(seqnames=c("chr1", "chr22"),
-                                    seqlengths=c(10^8, 10^8),
-                                    isCircular=c(FALSE, FALSE),
-                                    genome="toy")
+
+
+toySeqInfo <- GenomeInfoDb::Seqinfo(seqnames = c("chr1", "chr22"),
+                                    seqlengths = c(10^8, 10^8),
+                                    isCircular = c(FALSE, FALSE),
+                                    genome = "toy")
 
 test_that("parseLoopsRao works with example file", {
 
   gi <- parseLoopsRao(exampleLoopFile)
 
-  df <- read.table(exampleLoopFile, header=TRUE)
+  df <- read.table(exampleLoopFile, header = TRUE)
 
   expect_equal(length(gi),  nrow(df))
   expect_true(all(
@@ -25,5 +27,24 @@ test_that("parseLoopsRao works toy genome as seqinfo", {
   gi <- parseLoopsRao(exampleLoopFile, seqinfo=toySeqInfo)
 
   expect_identical(GenomeInfoDb::seqinfo(gi), toySeqInfo)
+
+})
+
+test_that("parseLoopsTang2015 works with example file", {
+
+  exampleLoopTang2015File <- system.file(
+    "extdata",
+    "ChIA-PET_GM12878_Tang2015.chr22_1-18000000.clusters.txt",
+    package = "chromloop")
+
+  gi <- parseLoopsTang2015(exampleLoopTang2015File)
+
+  df <- read.table(exampleLoopTang2015File, header = FALSE)
+
+  expect_equal(length(gi),  nrow(df))
+  expect_true(all(
+    GenomicRanges::seqnames(InteractionSet::regions(gi)) == "chr22"
+  ))
+  expect_equal(gi$score, df$V7)
 
 })
