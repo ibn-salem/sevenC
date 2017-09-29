6f8139df141b5e60f75dadd10ccf8c1c48cd10db
ibn-salem <jonas.ibn-salem@gmx.de>
2017-09-29 14:52:59 +0200

    add predLoops() function

--- R/prediction.R
+++ R/prediction.R
@@ -1,11 +1,11 @@
 
-
 #' Predict interaction probability using logistic regresstion model.
 #'
-#' @param data  A data.frame with predictor variables
-#' @param formula A modelling formula. All predictor variables should be
-#'   availabel in the data frame.
-#' @param betas A vector with parameter estimates for predictor variables
+#' @param data  A data.frame like object with predictor variables.
+#' @param formula A \code{\link[stats]{formula}}. All predictor variables should
+#'   be availabel in the data frame.
+#' @param betas A vector with parameter estimates for predictor variables. They
+#'   should be in the same order as variables in \code{formula}.
 #'
 #' @return A numeric vector with interaction probabilites for each ovservation
 #'   in \code{df}. NAs are produced for NAs in \code{df}.
@@ -23,4 +23,48 @@ pred_logit <- function(data, formula, betas){
   pred <- boot::inv.logit(pred)
 
   return(pred)
+}
+
+#' Predict looping interaction probability.
+#'
+#' @param gi A \code{\link[InteractionSet]{GInteractions}} object with coverage
+#'   corealtion and genomic features in metadata columns. See
+#'   \link{prepareCandidates} and \link{addCor} to build it.
+#' @param formula A \code{\link[stats]{formula}}. All predictor variables should
+#'   be availabel in the in metadat colums of \code{gi}. If NULL, the following
+#'   default formular is used: \code{~ dist + strandOrientation + score_min +
+#'   chip}.
+#' @param betas A vector with parameter estimates for predictor variables. They
+#'   should be in the same order as variables in \code{formula}.
+#' @param colname A \code{character} as column name of new metadata colum in
+#'   \code{gi} for predictions.
+#'
+#' @return A \code{\link[InteractionSet]{GInteractions}} as \code{gi} with an
+#'   additional metadata colum holdin the predicted looping probability.
+#'
+#' @seealso \code{\link{prepareCandidates}}, \code{\link{addCor}},
+#'   \code{\link{pred_logit}}
+#' @export
+#'
+predLoops <- function(gi, formula = NULL, betas = NULL, colname = "pred"){
+
+  # if no estimates are given, use the default parameters
+  if (is.null(betas)) {
+    betas <- modelBest10Avg$estimate
+  }
+
+  # if no formula is given, use default formula
+  if (is.null(formula)) {
+    formula <- stats::as.formula(
+      " ~ dist + strandOrientation + score_min + chip"
+    )
+  }
+
+  # predict interaction
+  pred <- pred_logit(S4Vectors::mcols(gi), formula, betas)
+
+  # add prediction as new metadata column
+  S4Vectors::mcols(gi)[, colname] <- pred
+
+  return(gi)
 }
--- tests/testthat/test-prediction.R
+++ tests/testthat/test-prediction.R
@@ -1,5 +1,16 @@
 context("predictions")
 
+
+# prepare gi from chr22 exampple data set
+exampleGI <- prepareCandidates(motif.hg19.CTCF.chr22, scoreColname = "sig")
+
+exampleBigWig <- system.file(
+  "extdata", "GM12878_Stat1.chr22_1-18000000.bigWig", package = "chromloop")
+
+exampleGI <- addCor(exampleGI, exampleBigWig)
+
+
+
 # Tests -------------------------------------------------------------------
 
 test_that("pred_logit gives same result as predict.glm()", {
@@ -15,5 +26,35 @@ test_that("pred_logit gives same result as predict.glm()", {
   names(pred_glm) <- NULL
 
   expect_equal(pred, pred_glm)
+})
+
+
+test_that("pred_logit works on chr22 example data", {
+
+  # run prediction
+  pred <- pred_logit(
+    S4Vectors::mcols(exampleGI),
+    stats::as.formula(" ~ dist + strandOrientation + score_min + chip"),
+    modelBest10Avg$estimate
+  )
+
+  expect_true(is.numeric(pred))
+  expect_equal(length(pred), length(exampleGI))
+  expect_true(all(pred <= 1 & pred >= 0, na.rm = TRUE))
+
+})
+
+test_that("predLoops works with default parameters", {
+
+  # run prediction
+  predGI <- predLoops(exampleGI)
+
+  expect_true(is.numeric(predGI$pred))
+  expect_equal(length(predGI), length(exampleGI))
+  expect_equal(
+      S4Vectors::mcols(predGI)[, -ncol(S4Vectors::mcols(predGI))],
+      S4Vectors::mcols(exampleGI))
+  expect_true(all(predGI$pred <= 1 & predGI$pred >= 0, na.rm = TRUE))
+
 })
 
